0f8a01bbeb8a18ca710889e4166b5395796b9d60
leebock <lbock@esri.com>
2022-03-25 11:03:43 -0400

    added comments and updated path for data files

--- driver.ps1
+++ driver.ps1
@@ -1,7 +1,11 @@
 $Path = "C:\Lee\repos\primary\tornado-dashboard"
-$TextFile_Files_With_GA = ".\files-w-ga.txt"
-$TextFile_Files_Without_Adobe = ".\files-wo-adobe.txt"
+$TextFile_Files_With_GA = "..\data\files-w-ga.txt"
+$TextFile_Files_Without_Adobe = "..\data\files-wo-adobe.txt"
 $CreateBackup = $false
+
+#***********************************************************
+#********** Search for files w/ Google Analytics ***********
+#***********************************************************
 
 Write-Host("Searching for files that contain Google Analytics...")
 
@@ -16,10 +20,18 @@ $measure = Get-Content $TextFile_Files_With_GA | Measure-Object -Line
 
 Write-Host("Found "+$measure.Lines+" files that contain Google Analytics.")
 
+#***********************************************************
+#********************* Back up files  **********************
+#***********************************************************
+
 if ($CreateBackup) {
     Write-Host("Creating backup files...")
     .\create-backups.ps1 -Input_File $TextFile_Files_With_GA    
 }
+
+#***********************************************************
+#**** Filter for files that don't have Adobe Analytics *****
+#***********************************************************
 
 Write-Host("Filtering for files that don't have Adobe Analytics...")
 
@@ -39,3 +51,9 @@ Write-Host("Found "+$measure.Lines+" files that DON'T contain Adobe Analytics.")
 foreach($line in (Get-Content $TextFile_Files_Without_Adobe)) {
     Write-Host($line)
 }
+
+#***********************************************************
+#************ For non Adobe files, insert Adobe ************
+#***********************************************************
+
+
